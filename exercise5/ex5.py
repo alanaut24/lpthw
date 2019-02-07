9f4f950f33aba5750a8b9e8f094301a2625efa1a
alanaut24 <alan@innospce.com>
2019-02-06 23:53:32 -0600

    Created Dir 5 and ex5.py file

---
+++ exercise5/ex5.py
@@ -0,0 +1,10 @@
+my_name = 'Alan W. Hurt Jr.'
+my_age = '34'
+my_height = '69'
+my_weight = '202'
+my_eyes = 'Hazel'
+my_teeth = 'White'
+my_hair = 'Brown'
+
+print(f"Let's talk about {my_name}.")
+print(f"He)
