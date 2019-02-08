dc149599287c7a10d71514026779f8938c7a4931
alanaut24 <alan@innospce.com>
2019-02-07 21:37:08 -0600

    Created dir exercise6 and ex6.py file

--- exercise5/ex5.py
+++ exercise5/ex5.py
@@ -1,18 +1,18 @@
-my_name = 'Alan W. Hurt Jr.'
-my_age = 34
-my_height = 69
-my_weight = 204
-my_eyes = 'Hazel'
-my_teeth = 'White'
-my_hair = 'Brown'
+name = 'Alan W. Hurt Jr.'
+age = 34
+height = 69
+weight = 204
+eyes = 'Hazel'
+teeth = 'White'
+hair = 'Brown'
 
-print(f"Let's talk about {my_name}.")
-print(f"He's {my_height} inches tall.")
-print(f"He's {my_weight} pounds heavy.")
+print(f"Let's talk about {name}.")
+print(f"He's {height} inches tall.")
+print(f"He's {weight} pounds heavy.")
 print("Actually that's not that heavy.")
-print(f"He's got {my_eyes} and {my_hair}.")
-print(f"His teeth are usually {my_teeth} depending on the coffe.")
+print(f"He's got {eyes} eyes and {hair} hair.")
+print(f"His teeth are usually {teeth} depending on the coffe.")
 
 # this line is tricky, try to get it exactly right
-total = my_age + my_height + my_weight
-print(f"If I add {my_age}, {my_height}, and {my_weight} I get {total}.")
+total = age + height + weight
+print(f"{name} weighs {weight * 2.2} pounds, he's {height * 0.39370} inches tall and (unfortunately for him) is still {age} years old.")
