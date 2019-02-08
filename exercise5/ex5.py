2289d22e02534466e486838b3f23d4b08a850f33
alanaut24 <alan@innospce.com>
2019-02-07 20:36:49 -0600

    Created Dir exercise5 and ex5.py file

--- exercise5/ex5.py
+++ exercise5/ex5.py
@@ -1,10 +1,18 @@
 my_name = 'Alan W. Hurt Jr.'
-my_age = '34'
-my_height = '69'
-my_weight = '202'
+my_age = 34
+my_height = 69
+my_weight = 204
 my_eyes = 'Hazel'
 my_teeth = 'White'
 my_hair = 'Brown'
 
 print(f"Let's talk about {my_name}.")
-print(f"He)
+print(f"He's {my_height} inches tall.")
+print(f"He's {my_weight} pounds heavy.")
+print("Actually that's not that heavy.")
+print(f"He's got {my_eyes} and {my_hair}.")
+print(f"His teeth are usually {my_teeth} depending on the coffe.")
+
+# this line is tricky, try to get it exactly right
+total = my_age + my_height + my_weight
+print(f"If I add {my_age}, {my_height}, and {my_weight} I get {total}.")
