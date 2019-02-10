25d8926f4cf5cfd60e00ce5db239708baa0d158a
alanaut24 <alan@innospce.com>
2019-02-09 23:36:29 -0600

    Addeed comments to each line

--- exercise8/ex8.py
+++ exercise8/ex8.py
@@ -4,7 +4,11 @@ formatter = "{} {} {} {}"
 print(formatter.format(1, 2, 3, 4))
 print(formatter.format("one", "two", "three", "four"))
 print(formatter.format(True, False, False, True))
+
+# No quotes when calling variables
 print(formatter.format(formatter, formatter, formatter, formatter))
+
+# Formatting for the array
 print(formatter.format(
     "Try your",
     "Own text here",
