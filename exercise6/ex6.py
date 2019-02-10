4a78192047575df686f680a41b2a595b110b4ef6
alanaut24 <alan@innospce.com>
2019-02-09 22:44:51 -0600

    Added comments to ex6.py file

--- exercise6/ex6.py
+++ exercise6/ex6.py
@@ -1,8 +1,18 @@
+# creates variable types of people and assigns value 10
 types_of_people = 10
+# Sets variable x to string with an embedded format string. Format strings
+# are way to insert (embed) a thing within a string such that the final
+# output will include the thing or whatever operation the thing is part of.
 x = f"There are {types_of_people} types of people."
 
+# sets variable binary to binary = slightly redundant but necessary
 binary = "binary"
+
+# sets variable do_not to don't
 do_not = "don't"
+
+# set variable y to string with two embedded format strings
+# binary and do_not
 y = f"Those who know {binary} and those who {do_not}."
 
 print(x)
