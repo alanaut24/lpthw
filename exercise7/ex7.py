f9bdd067c53912233daeae39fc1077dcd53afd3d
alanaut24 <alan@innospce.com>
2019-02-09 23:11:03 -0600

    Created Dir exercise8 and ex8.py file

--- exercise7/ex7.py
+++ exercise7/ex7.py
@@ -1,7 +1,7 @@
 print("Mary had a little lamb.")
 print("It's fleece was white as {}".format('snow'))
-print("And everywheere that Mary went.")
-print("." * 10) # what'd that do?
+print("And everywhere that Mary went.")
+print("." * 10) # prints 10 full stops
 
 end1 = "C"
 end2 = "h"
@@ -16,6 +16,7 @@ end10 = "g"
 end11 = "e"
 end12 = "r"
 
-# watch end = ' ' at the  end. try removing it to see what happens
-print(end1 + end2 + end3 + end4 + end5 + end6, end =' ')
-print(end7 + end8 + end9 + end10 + end11 + end12)
+# A comma at the end of the line will tell the print to output the next
+# printout on the same line but  with a space aftr the  last variable
+print(end1 + end2 + end3 + end4 + end5 + end6, end=' ')
+print(end7 + end8 + end9 +end10 + end11 + end12)
