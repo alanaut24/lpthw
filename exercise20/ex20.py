052dbd750c49f99e7574d015c1f0bb3fd7f29d91
alanaut24 <alan@innospce.com>
2019-02-18 23:01:51 -0600

    Created text.txt file and finished ex20.py

--- exercise20/ex20.py
+++ exercise20/ex20.py
@@ -6,14 +6,14 @@ def print_all(f):
     print(f.read())
 
 def rewind(f):
-    print(f.seek(0))
+    f.seek(0)
 
 def print_a_line(line_count, f):
     print(line_count, f.readline())
 
 current_file = open(input_file)
 
-print("First, let's print the whole file:\n")
+print("First let's print the whole file:\n")
 
 print_all(current_file)
 
@@ -21,7 +21,7 @@ print("Now let's rewind, kind of like a tape.")
 
 rewind(current_file)
 
-print("Let's print three lines.")
+print("Let's print three lines:")
 
 current_line = 1
 print_a_line(current_line, current_file)
