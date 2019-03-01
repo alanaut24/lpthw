f43bc3d202dcaa8e9d2d3f5a2b7f0eebba7346f6
alanaut24 <alan@innospce.com>
2019-02-28 22:47:52 -0600

    Finished coding ex24 and trying to debug

--- exercise24/ex24.py
+++ exercise24/ex24.py
@@ -11,3 +11,25 @@ nor comprehend passion from intuition
 and requires an explanation
 \n\t\twhere there is none
 """
+
+print("----------")
+print(poem)
+print("----------")
+
+five = 10 - 2 + 3 - 6
+print(f"This should be five: {five}")
+
+def secret_formula(started):
+    jelly_beans = started * 500
+    jars = jelly_beans / 1000
+    crates = jars * 100
+    return jelly_beans, jars, crates
+
+
+start_point = 10000
+beans, jars, crates = secret_formula(start_point)
+
+# remember that this is another way to format a string
+print("With a starting point of: {}".format(start_point))
+# it's just like with an f" string
+print("We'd have {} beans, {} jars, and {} crates.".format(secret_formula))
