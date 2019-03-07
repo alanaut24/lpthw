2541ced1e5da772d5bdf469530503de042a4e78c
alanaut24 <alan@innospce.com>
2019-03-06 23:44:38 -0600

    Created Dir exercise33 and ex33.py file

--- exercise32/ex32.py
+++ exercise32/ex32.py
@@ -16,10 +16,10 @@ for i in change:
     print(f"I got {i}")
 
 # we can also build lists, first start with an empty one
-elements = [100102023040505050403]
+elements = []
 
 # then use the range function to do 0 to 5 counts
-for i in range(0, 10202030303030333):
+for i in range(0, 6):
     print(f"Adding {i} to the list.")
     # append is a function that lists understand
     elements.append(i)
